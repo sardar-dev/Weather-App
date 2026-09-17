5d1f2f8be1cf299e1584b575e47da6c210ab475f
sardar <sardar6g@gmail.com>
2026-09-17 13:05:21 +0500

    Refactor input handling and improve localStorage integration in app.js

--- app.js
+++ app.js
@@ -15,21 +15,23 @@ let empty = document.querySelector(".empty");
 let notfound = document.querySelector(".notfound");
 let all = document.querySelectorAll(".all");
 
-let input = document.querySelector(".input")
+let input = document.querySelector(".input");
 
-input.value = localStorage.getItem("place") || ""
-
+input.value = localStorage.getItem("place") || "";
 
 // function for onsubmiting form
 async function checkWeather(event) {
   try {
     event.preventDefault();
-    
+
     // saving input value
     let location = document.querySelector(".input").value.trim();
-    
+
     // saving input value into the localStorage
-    localStorage.setItem("place" , input.value)
+    localStorage.setItem("place", input.value);
+
+    // removing previous not found msg
+    notfound.innerHTML = "";
 
     // handling empty input
     if (location === "") {
